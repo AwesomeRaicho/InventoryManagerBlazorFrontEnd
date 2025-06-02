e85d423c451f808149813a0e2f8cac1efd5bbbef
Ricardo Araujo <ricardo.dev0188@gmail.com>
2025-06-02 14:22:31 -0700

    create login token model in login page

---
+++ InventoryManagerFrontEnd/Models/TokenRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagerFrontEnd.Models
+{
+    public class TokenRequest
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+    }
+}
